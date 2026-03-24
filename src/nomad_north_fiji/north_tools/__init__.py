4077d325795781c83c64c063e3305e0a044d8e94
mkuehbach <markus.kuehbach@physik.hu-berlin.de>
2026-03-24 11:00:44 +0100

    correct init

--- src/nomad_north_fiji/north_tools/__init__.py
+++ src/nomad_north_fiji/north_tools/__init__.py
@@ -2,14 +2,18 @@ from nomad.config.models.north import NORTHTool
 from nomad.config.models.plugins import NorthToolEntryPoint
 
 fiji = NORTHTool(
-    short_description='Jupyter Notebook server in NOMAD NORTH for NOMAD plugin nomad-north-fiji.',
+    short_description='Use FIJI to visualize and analyze your images in NOMAD.',
     image='ghcr.io/fairmat-nfdi/nomad-north-fiji:main',
-    description='Jupyter Notebook server in NOMAD NORTH for NOMAD plugin nomad-north-fiji.',
+    description="""### **FIJI**:
+
+    [Fiji Is Just ImageJ to visualize and analyze your images in NOMAD.](https://imagej.net/learn/)
+
+    [Research article about the software](https://doi.org/10.1038/nmeth.2019)""",
     external_mounts=[],
-    file_extensions=['ipynb'],
+    file_extensions=['tif, tiff, jpeg, png, gif, bmp, avi'],
     icon='https://raw.githubusercontent.com/FAIRmat-NFDI/nomad-north-fiji/main/src/nomad_north_fiji/north_tools/fiji/fiji.png',
     image_pull_policy='Always',
-    default_url='/lab',
+    default_url='/desktop',
     maintainer=[{'email': 'markus.kuehbach@physik.hu-berlin.de', 'name': 'Markus Kühbach'}],
     mount_path='/home/jovyan',
     path_prefix='lab/tree',

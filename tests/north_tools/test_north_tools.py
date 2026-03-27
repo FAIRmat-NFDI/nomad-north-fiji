348cd69d72694a5181d68609e0da4e59ff30a66b
mkuehbach <markus.kuehbach@physik.hu-berlin.de>
2026-03-27 14:21:58 +0100

    test

--- tests/north_tools/test_north_tools.py
+++ tests/north_tools/test_north_tools.py
@@ -1,9 +1,7 @@
 def test_importing_north_tool():
     # this will raise an exception if pydantic model validation fails
-    from nomad_north_fiji.north_tools import north_entry_point
+    from nomad_north_fiji.north_tools import fiji
 
-    expected_id = 'nomad-north-fiji-fiji'
-    assert (
-        north_entry_point.id_url_safe == expected_id
-        or north_entry_point.id == 'nomad-north-nomad-north-fiji'
-    ), 'NORTHTool entry point has incorrect id or id_url_safe'
+    assert fiji.id_url_safe == 'fiji' or fiji.id == 'nomad-north-fiji', (
+        'NORTHTool entry point has incorrect id or id_url_safe'
+    )
